6f03c71967d3b89e818b4a8638784c362b1c64a4
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-11 08:54:25 -0800

    Initial Commit

---
+++ src/main/java/com/boydlever/auth/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.boydlever.auth.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.boydlever.auth.models.User;
+
+public interface UserRepository extends CrudRepository <User,Long>{
+	Optional<User> findByEmail(String email);
+}
