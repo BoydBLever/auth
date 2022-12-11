91cf60a1d81e03e97529b07c55d49e5cd5aec0c9
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-11 10:47:14 -0800

    Updated UserController.java

--- src/main/java/com/boydlever/auth/controllers/UserController.java
+++ src/main/java/com/boydlever/auth/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.boydlever.auth.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.boydlever.auth.models.LoginUser;
 import com.boydlever.auth.models.User;
@@ -16,5 +22,46 @@ public class UserController {
 		model.addAttribute("newLogin", new LoginUser());
 		return "logreg.jsp";
 	}
-	
+	@PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // TO-DO Later -- call a register method in the service 
+        // to do some extra validations and create a new user!
+        
+        if(result.hasErrors()) {
+            // Be sure to send in the empty LoginUser before 
+            // re-rendering the page.
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+    
+        return "redirect:/home";
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // Add once service is implemented:
+        // User user = userServ.login(newLogin, result);
+    
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+    
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+    
+        return "redirect:/home";
+    }
+    
+}copy
+
 }
